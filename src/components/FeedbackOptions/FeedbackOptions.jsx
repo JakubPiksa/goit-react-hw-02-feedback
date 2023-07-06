01ee8076d786112af49ec287d1a700073aa97ccf
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-06 22:14:47 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import styles from './feedbackOptions.module.css';
+import css from './feedbackOptions.module.css';
 
 
-const FeedbackOptions = () => {
+const FeedbackButtons = () => {
     return (
         <div>
             <h1>Please leave feedback</h1>
