2796c66210715f72154aca60626224aff4ab131d
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-06 22:10:24 +0200

    okay getting started

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,20 @@ import React, { useState } from "react";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 
 
+
 export const App = () => {
   const [feedback, setFeedback] = useState({
-  good: 0,
-  neutral: 0,
-  bad: 0
+    good: 0,
+    neutral: 0,
+    bad: 0,
   });
+  
   return (
     <div>
       <FeedbackOptions />
     </div>
-  )
+  );
+  
 };
-
 
 
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import styles from './feedbackOptions.module.css';
 
 
-class FeedbackOptions extends Component {
-    render() {
-        return <h1>TEst</h1>
-    }
+const FeedbackOptions = () => {
+    return (
+        <div>
+            <h1>Please leave feedback</h1>
+            <button>Good</button>
+            <button>Neutral</button>
+            <button>Bad</button>
+            <h2>Statistics</h2>
+            <ul>
+                <li>Good: 0</li>
+                <li>Neutral: 0</li>
+                <li>Bad: 0</li>
+            </ul>
+        </div>
+    );
 };
 
 export default FeedbackOptions;
