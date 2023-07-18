39a10855c597f4bea78901359a4dedf5ccac66e2
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-18 10:55:54 +0200

    lets see agaibn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
-import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
-import Statistics from './Statistics/Statistics';
+import './App.css';
+import user from './user/user.json';
+import friendsData from './friends/friends.json';
+import data from './data/data.json';
+import transactionsData from './transactions/transactions.json';
+import { Profile } from './user/user.jsx';
+import { FriendList } from './friends/friends.jsx';
+import { Statistics } from './data/data.jsx';
+import { TransactionHistory } from './transactions/transactions.jsx';
 
-const App = () => {
-  const [feedbackCounter, setFeedbackCounter] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  });
-//ghpages jest ble
-  const [showStatistics, setShowStatistics] = useState(false);
-
-  const handleFeedback = (type) => {
-    setFeedbackCounter((prevCounts) => ({
-      ...prevCounts,
-      [type]: prevCounts[type] + 1
-    }));
-    setShowStatistics(true);
-  };
-
+function App() {
   return (
     <div>
-      <h1>Please Leave Feedback</h1>
-      <FeedbackOptions handleFeedback={handleFeedback} />
-      {showStatistics && <Statistics feedbackCounter={feedbackCounter} />}
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} />
+      <FriendList friends={friendsData} />
+      <TransactionHistory transactions={transactionsData} />
     </div>
   );
-};
+}
 
 export default App;
